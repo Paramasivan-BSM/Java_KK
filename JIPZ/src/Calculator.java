3a9ff4ca0bb0faa7227c78048de00953c3a8b21a
paramasivan <paramasivan645@gmail.com>
2024-06-07 21:20:00 +0530

    Calculator Done!

--- JIPZ/src/Calculator.java
+++ JIPZ/src/Calculator.java
@@ -1,97 +1,35 @@
+
 import java.util.Scanner;
 
- class Calulator {
+class Calculator {
     public static void main(String[] args) {
-        System.out.println("\n");
-        Scanner Input = new Scanner(System.in);
-        int Result ;
+        Scanner input = new Scanner(System.in);
+        int result = 0; // Initialize the result variable
 
-while (true) {
-    System.out.println("Enter The Operator :");
+        while (true) {
+            System.out.println("Enter the Operator (+, -, *, %):");
+            String operator = input.next();
 
-    String Operator = Input.next();
-     
-   
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("%")) {
+                System.out.println("Enter two integers:");
+                int value1 = input.nextInt();
+                int value2 = input.nextInt();
 
+                if (operator.equals("+")) {
+                    result = value1 + value2;
+                } else if (operator.equals("-")) {
+                    result = value1 - value2;
+                } else if (operator.equals("*")) {
+                    result = value1 * value2;
+                } else if (operator.equals("%")) {
+                    result = value1 % value2;
+                }
 
-if (Operator.equals("+") || Operator.equals("-") || Operator.equals("*") || Operator.equals("%")) {
-
-    int Value1 = Input.nextInt();
-     int Value2 = Input.nextInt();
-
-    if(Operator == "+"){
-       
-        Result = Value1 + Value2;
-        
-        
-
-     }
-
-     else if (Operator == "-" ){
-        Result = Value1 - Value2;
-        
-     }
-    else if (Operator == "*") {
-        Result = Value1 * Value2;
-
-     }
-     else if (Operator == "%") {
-        Result = Value1 % Value2;
-        
-     }
-     System.out.println("Result: " + Result);
-    
-}
-else{
-   
-    System.out.println("Invalid Operator");
-    break;
-}
-
-
-    
-
-     
-     
-     
-
-    
-}
-
-
+                System.out.println("Result: " + result);
+            } else {
+                System.out.println("Invalid Operator. Please enter a valid operator.");
+                input.close();
+            }
+        }
     }
-    
 }
-
-// import java.util.Scanner;
-
-// public class Calculator {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         int result = 0; // Initialize the result variable
-
-//         while (true) {
-//             System.out.println("Enter the Operator (+, -, *, %):");
-//             String operator = input.next();
-
-//             if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("%")) {
-//                 int value1 = input.nextInt();
-//                 int value2 = input.nextInt();
-
-//                 if (operator.equals("+")) {
-//                     result = value1 + value2;
-//                 } else if (operator.equals("-")) {
-//                     result = value1 - value2;
-//                 } else if (operator.equals("*")) {
-//                     result = value1 * value2;
-//                 } else if (operator.equals("%")) {
-//                     result = value1 % value2;
-//                 }
-
-//                 System.out.println("Result: " + result);
-//             } else {
-//                 System.out.println("Invalid Operator. Please enter a valid operator.");
-//             }
-//         }
-//     }
-// }
